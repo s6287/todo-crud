e2901abef6ff0a0a06940e44827c2dbe3d52edce
s6287 <shivamsinghsrs@gmail.com>
2025-10-09 22:58:38 +0530

    Filter implemented

--- src/Component/Todo.jsx
+++ src/Component/Todo.jsx
@@ -4,6 +4,8 @@ function Todo() {
     const [List, addList] = useState([]);
     const [inputvalue, readInputValue] = useState("");
     const [editIndex, seteditIndex] = useState(null);
+    const [filter, setFilter] = useState("all"); // "all" | "active" | "completed"
+
 
     function catchinputfeild(e) {
         readInputValue(e.target.value)
@@ -11,7 +13,7 @@ function Todo() {
 
     function addtask() {
         if (inputvalue.trim() === "") return; // optional: prevent empty tasks
-        addList([...List, inputvalue]);
+        addList([...List, { text: inputvalue, completed: false }]);
         readInputValue("")
     }
 
@@ -25,22 +27,42 @@ function Todo() {
     function editTask(index) {
         seteditIndex(index);
         // seteditValue(List[index])
-        readInputValue(List[index])
+        readInputValue(List[index].text)
     }
 
     function saveTask() {
         let copy = [...List];
-        copy[editIndex] = inputvalue;
+        copy[editIndex].text = inputvalue;
         addList(copy)
         readInputValue("");
         seteditIndex(null);
     }
 
+    function completemark(index) {
+        const updatedlist = [...List];
+        if (updatedlist[index].completed === true) {
+            updatedlist[index].completed = false;
+        } else {
+            updatedlist[index].completed = true;
+        }
+        addList(updatedlist);
+    }
+
+    const filteredList = List.filter(item => {
+        if (filter === "active") return item.completed === false;
+        if (filter === "completed") return item.completed === true;
+        return true; // for "all"
+    });
 
     return (
         <>
 
             <div className="flex items-center gap-4 mb-4">
+
+                <button onClick={() => setFilter("active")} className="px-3 py-1 bg-blue-200 rounded">Active Task</button>
+                <button onClick={() => setFilter("completed")} className="px-3 py-1 bg-green-200 rounded">Completed Task</button>
+                <button onClick={() => setFilter("all")} className="px-3 py-1 bg-gray-200 rounded">All Tasks</button>
+
                 <input
                     onChange={catchinputfeild}
                     type="text"
@@ -49,28 +71,37 @@ function Todo() {
                     className="flex-1 px-4 py-2 border-2 border-amber-500 rounded-lg focus:outline-none focus:ring-2 focus:ring-amber-400 placeholder:text-gray-400"
                 />
                 <button onClick={editIndex !== null ? saveTask : addtask} className="px-6 py-2 bg-amber-500 text-white font-medium rounded-lg hover:bg-amber-600 transition-colors duration-200">
-                    {editIndex != null ? "Save Task" : "Add Task"}
+                    {editIndex != null ? "✔️" : "➕"}
                 </button>
-            </div>
+            </div >
             {/* List showing  */}
-            <div className="mt-4">
+            < div className="mt-4" >
                 <ul className="space-y-2">
-                    {List.map((item, index) => (
+                    {filteredList.map((item, index) => (
                         <li key={index}>
                             <div className="flex justify-between items-center border border-amber-400 rounded-lg px-4 py-2 bg-white shadow-sm">
-                                <span className="text-gray-800 font-medium">{item}</span>
-                                <button onClick={function () { deleteTask(index) }} className="text-red-500 hover:text-red-700 font-medium">
-                                    Delete Task
+                                <span
+                                    className={`ml-6 text-gray-800 font-medium ${item.completed ? "line-through text-gray-400" : ""
+                                        }`}
+                                >
+                                    {item.text}
+                                </span>
+                                <button onClick={function () { deleteTask(index) }} className=" ml-6 text-red-500 hover:text-red-700 font-medium">
+                                    🗑️
                                 </button>
 
-                                <button onClick={function () { editTask(index) }} className="text-red-500 hover:text-red-700 font-medium">
-                                    Edit Task
+                                <button onClick={function () { editTask(index) }} className=" ml-6 text-red-500 hover:text-red-700 font-medium">
+                                    📝
+                                </button>
+
+                                <button onClick={function () { completemark(index) }} className=" ml-6 text-red-500 hover:text-red-700 font-medium">
+                                    {item.completed ? "Mark Not completed" : "completed"}
                                 </button>
                             </div>
                         </li>
                     ))}
                 </ul>
-            </div>
+            </div >
 
 
         </>
